e23d958a64ab71f272735ee77484a8c5b3efe90b
Maksym <b.orlov7709@gmail.com>
2024-10-12 19:48:43 +0300

    upd 3d model

--- src/components/Computers/Computers.jsx
+++ src/components/Computers/Computers.jsx
@@ -1,27 +1,30 @@
-import React, {Suspense, useEffect, useState} from "react";
-import {Canvas} from "@react-three/fiber";
-import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
-import {EffectComposer, Bloom} from '@react-three/postprocessing';
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { EffectComposer, Bloom } from '@react-three/postprocessing';
+import './_Computers.scss';
 
-const Computers = ({isMobile}) => {
+const Computers = ({ isMobile }) => {
     const computer = useGLTF("./desktop_pc/scene.gltf");
 
     return (
         <mesh>
-            <hemisphereLight intensity={0.15} groundColor="black"/>
+            {/* Общая освещенность */}
+            <ambientLight intensity={0.3} />
+            <hemisphereLight intensity={0.15} groundColor="black" />
             <spotLight
-                position={[-20, 50, 10]}
-                angle={0.12}
+                position={[-20, 70, 10]}
+                angle={1.7}
                 penumbra={1}
-                intensity={1}
+                intensity={5}
                 castShadow
                 shadow-mapSize={1024}
             />
-            <pointLight intensity={20}/>
+            <pointLight intensity={10} />
             <primitive
                 object={computer.scene}
                 scale={isMobile ? 0.7 : 0.75}
-                position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+                position={isMobile ? [0, 0, -2.2] : [0, 0, -1.5]}
                 rotation={[-0.01, -0.2, -0.1]}
             />
         </mesh>
@@ -51,22 +54,20 @@ const ComputersCanvas = () => {
             frameloop='demand'
             shadows
             dpr={[1, 2]}
-            camera={{position: [20, 10, 2], fov: 5}}
-            gl={{preserveDrawingBuffer: true}}
-        >
+            camera={{ position: [20, 11, 2], fov: 25 }}
+            gl={{ preserveDrawingBuffer: true }}>
             <Suspense>
                 <OrbitControls
                     enableZoom={false}
                     maxPolarAngle={Math.PI / 2}
                     minPolarAngle={Math.PI / 2}
                 />
-                <Computers isMobile={isMobile}/>
+                <Computers isMobile={isMobile} />
                 <EffectComposer>
-                    <Bloom intensity={0.5}/>
+                    <Bloom intensity={0.5} />
                 </EffectComposer>
             </Suspense>
-
-            <Preload all/>
+            <Preload all />
         </Canvas>
     );
 };
